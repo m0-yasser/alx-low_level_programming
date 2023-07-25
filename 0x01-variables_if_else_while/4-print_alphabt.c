b53a8462930289252c762e4dc5f140f251bde1e8
Mohamed_yasser <my579267485@gmail.com>
2023-07-24 22:54:51 -0700

    alphABET

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - entry point
+ * Return: 0
+ */
+int main(voi)
+{
+	char ch = 'a';
+	while (ch <= 'z')
+	{
+		if (ch == 'q' || ch = 'e')
+			ch++;
+		putchar(ch);
+			ch++;
+	}
+	putchar('\n');
+}
