6480cff19ed9d6360e0093715ee500c087918c19
Mohamed_yasser <my579267485@gmail.com>
2023-07-25 13:19:40 -0700

    a

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - entry point
+ * Return: 0
+ */
+int main(void)
+{
+	int num = 48;
+
+	while (num <= 57)
+	{
+		putchar(num);
+	num++;
+	}
+	putchar('\n');
+	return (0);
+}
