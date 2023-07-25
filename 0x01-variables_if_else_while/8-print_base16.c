bee1f5ca77b8de67a5a23041fb9dc5951e82e3b0
Mohamed_yasser <my579267485@gmail.com>
2023-07-25 13:51:39 -0700

    a

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+ * main - entry point
+ * Return: 0
+ */
+int main(void)
+{
+	int digit = 48;
+
+	while (digit <= 102)
+	{
+		putchar(digit)
+		if (digit >= 58 && digit <= 96)
+			digit++;
+		digit++;
+	}
+	putchar('\n');
+	return (0);
+}
