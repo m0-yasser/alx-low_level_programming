698a961d8fe85d3062f75e1218d50155bbbd0023
Mohamed_yasser <my579267485@gmail.com>
2023-08-04 10:08:26 -0700

    first task

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet - Entry point
+ * Return:0
+ */
+void print_alphabet(void)
+{
+	char x = 'a';
+
+	while (x <= 'z')
+	{
+		putchar(x);
+		x++;
+
+	}
+	putchar('\n');
+	return (0);
+
+}
