8e3869082d9792f46722bb4fb1767c5de2d4aff4
Mohamed_yasser <my579267485@gmail.com>
2023-08-05 09:06:21 -0700

    c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * times_table - print the time table
+ * Return: nothing
+ */
+
+void times_table(void)
+{
+    int x, y, z;
+    
+    for (x = 0; x <= 9; x++)
+    {
+        for (y = 0; y <= 9; y++)
+        {
+            printf("%d", x*y);
+            printf(", ");
+        }
+        printf("\n");
+    }
+}
