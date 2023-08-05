389ae2ddb66984da6ab53b793c858cefb1790953
Mohamed_yasser <my579267485@gmail.com>
2023-08-05 09:09:18 -0700

    c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,15 +7,15 @@
 
 void times_table(void)
 {
-    int x, y, z;
-    
-    for (x = 0; x <= 9; x++)
-    {
-        for (y = 0; y <= 9; y++)
-        {
-            printf("%d", x*y);
-            printf(", ");
-        }
-        printf("\n");
-    }
+	int x, y;
+
+	for (x = 0; x <= 9; x++)
+	{
+		for (y = 0; y <= 9; y++)
+		{
+			printf("%d", x * y);
+			printf(", ");
+		}
+		printf("\n");
+	}
 }
