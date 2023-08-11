171984c4154fe5339c7cf224a51ad8062f04253a
Mohamed_yasser <my579267485@gmail.com>
2023-08-11 06:29:23 -0700

    recursion

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - return length of string
+ * @s: input string
+ * Return: length
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == 0)
+		return;
+
+	return (1 + _strlen_recursion(1 + s));
+}
