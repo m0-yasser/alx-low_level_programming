b1d76d77890273bf2cc7c92c83ad907fcdb156ed
Mohamed_yasser <my579267485@gmail.com>
2023-08-12 09:38:03 -0700

    recursion

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * square - return natural square root of a num
+ * @x: input number
+ * @y: root of number
+ *Return: -1 if failed
+ */
+
+int square(int x, int y)
+{
+	if (y * y = x)
+		return (b);
+	if (y * y > x)
+		return (-1);
+	return (square(x, y+1));
+}
+
+/**
+ * _sqrt_recursiom - return square
+ * @n: input number
+ * Return: 1
+ */
+
+int _sqrt_recursion(int n)
+{
+	return (square(n, 1));
+}
