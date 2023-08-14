b7a9a24ac41ad5436aa4516228f12acfdd00cebf
Mohamed_yasser <my579267485@gmail.com>
2023-08-14 13:29:29 -0700

    argc argv

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -9,12 +9,10 @@
 
 int main(int argc, char *argv[])
 {
-	int i = 0;
+	int i;
 
-	while (argc == 0)
-	{
-		printif("%s\n" argv[i]);
-		i--;
-	}
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
+
 	return (0);
 }
