feaf847acb7a713ec47de33f87ad56e04b460fb0
Mohamed_yasser <my579267485@gmail.com>
2023-08-21 13:05:00 -0700

    strct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,28 +1,23 @@
 #ifndef DOG_H
 #define DOG_H
-
 /**
- * struct dog - a dog
- * @naame: dog's name
- * @age: dog's age
- * @owner: dog's owner
+ * struct dog - list dates about pet
+ * @name: pet name
+ * @age: pet age
+ * @owner: pet owner name
  */
-
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
-
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 /**
  * dog - rename strucut with typedef
  */
-
 typedef struct dog dog_t;
-void print_dog(struct dog *d);
-void init_dog(struct dog *d, char *name, float age, char *owner);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-
 #endif
